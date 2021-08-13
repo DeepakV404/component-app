5006b95ee0aa14281d6a2982111b88cc6ed66411
Deepak <17deepakrv5@gmail.com>
2021-08-13 17:06:44 +0530

    Added a nested component within functional component

--- src/components/FunctionalComponent.js
+++ src/components/FunctionalComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import NstComponent from './NestedComponent';
 
 const FunctionalComponent = () => {
     return (
         <div>
             <h1>Hello!!</h1>
             <h2>This is a functional component</h2>
+            <NstComponent/>
         </div>
     )
 }
---
+++ src/components/NestedComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NestedComponent = () => {
+    return(
+        <div>
+            <h2>I am a nested component...</h2>
+        </div>
+    )
+}
+export default NestedComponent;
